ffe58f81c82c72a2e520f45276f44c1f2f70dfaf
Guillaume Pitot <guillaume@honeyinsurance.com>
2023-08-07 12:22:44 +1000

    add client

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: "./client",
+  build: {
+    // commonjsOptions: {
+    //   include: [/node_modules/],
+    // },
+    outDir: "../dist/api/public",
+  },
+  plugins: [react()],
+});
